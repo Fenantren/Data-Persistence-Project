b8424a68b7bfcbe6c8162abba090bb552ea38f99
Fenantren <p.paszko93@gmail.com>
2024-02-13 22:38:34 +0000

    Menu UI update
    Added Options and HighScore canvases

--- Assets/Scripts/MenuScripts/MainMenuHandler.cs
+++ Assets/Scripts/MenuScripts/MainMenuHandler.cs
@@ -7,10 +7,31 @@ using UnityEngine.UI;
 
 public class MainMenuHandler : MonoBehaviour
 {
+    [SerializeField] GameObject mainScreen;
+    [SerializeField] GameObject highScoreScreen;
+    private bool isMainActive;
     //Start game by loading game scene
     public void StartGame()
     {
         SceneManager.LoadScene(1);
     }
+    public void HighScoreMenu()
+    {
+        mainScreen.SetActive(false);
+        isMainActive = false;
 
+        highScoreScreen.SetActive(true);
+    }
+    public void BackToMenu()
+    {
+        if(isMainActive == false)
+        {
+            highScoreScreen.SetActive(false);
+            
+            mainScreen.SetActive(true);
+            isMainActive = true;
+        }
+        
+        
+    }
 }
